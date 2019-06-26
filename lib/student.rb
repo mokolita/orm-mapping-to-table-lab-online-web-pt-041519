24a7e688336a839d9a018b5e3d5cfb0501cda17f
Meg Galvez <meg@oakmaine.com>
2019-06-26 22:54:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,7 @@ class Student
   end 
   
   def self.drop_table
-    sql = <<-SQL DROP TABLE IF EXISTS students
+    sql = <<-SQL DROP TABLE IF EXISTS students SQL 
+      DB[:conn].execute(sql)
   end 
 end
